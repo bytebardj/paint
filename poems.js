1dc01ff644ddb1b41722fab81abebc31da82fc6b
joehaveamoment <joeinnz@hotmail.com>
2020-09-02 22:50:55 +1200

    poemtest

--- poems.js
+++ poems.js
@@ -1,3 +1,9 @@
+<!DOCTYPE javascript>
+<style type="text/css">
+body { font: 14px/1.3 verdana, arial, helvetica, sans-serif; }
+h1 { font-size:1.3em; }
+h2 { font-size:1.2em; }
+</style>
 var poems = [
 '送灵澈上人<br>刘长卿<br>苍苍竹林寺<br>杳杳钟声晚<br>荷笠带斜阳<br>青山独归远。<br>(斜阳 一作：夕阳)<br>',
 '456<br>'

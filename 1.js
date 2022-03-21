0395c65892e0b6593213429feea2f2f83acc3325
Joe <joeinnz@hotmail.com>
2022-03-22 09:59:32 +1300

    u

---
+++ 1.js
@@ -0,0 +1,13 @@
+ let animal = 'fly';
+
+ switch(animal) {
+    case 'dog' :
+     console.log('dog');
+     break
+    case 'cat':
+        console.log('cat');
+    break
+    default:
+    console.log('Neither dog nor cat')
+    break
+ }
